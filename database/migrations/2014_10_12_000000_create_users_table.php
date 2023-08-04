a6e55b2756ea28b321541b75afcbf5a70f98489e
MAÑANA <MAANA@MEDAPRCSGFSP035.FormacionSena.RED>
2023-08-04 10:20:41 -0500

    Routes, Controllers, Views & Migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,13 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->id();//Integer, Unsigned, Increment
+            $table->string('name');//Varchar de 255, se puede especificar menos ('name', 100)
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamps();//created_at updated_at
         });
     }
 
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CursoController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/', HomeController::class);
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(CursoController::class)->group(function(){
+    Route::get('cursos', 'index');
+    Route::get('cursos/create', 'create');
+    Route::get('cursos/{curso}', 'show');
 });
+
+
+/* Route::get('cursos/{curso}/{categoria?}', function ($curso,$categoria = null) {
+    if ($categoria) {
+        return "Bienvenidos al curso: $curso, de la categoria $categoria";
+    }else {
+        return "Bienvenidos al curso: $curso";
+    }
+}); */
